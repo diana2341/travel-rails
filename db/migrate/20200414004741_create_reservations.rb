e772a751c7f7ca579276cda58a089d94f563af1c
JohnsonNYC <jkow95@gmail.com>
2020-04-13 21:04:31 -0400

    Tables and Migrations

--- db/migrate/20200414004741_create_reservations.rb
+++ db/migrate/20200414004741_create_reservations.rb
@@ -1,7 +1,11 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :reservations do |t|
-
+      t.date :checkin
+      t.date :checkout
+      t.integer :listing_id
+      t.integer :guest_id
+      t.integer :confirmation_number
       t.timestamps
     end
   end
--- db/migrate/20200414004755_create_reviews.rb
+++ db/migrate/20200414004755_create_reviews.rb
@@ -1,7 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.integer :rating
+      t.string :description
+      t.integer :reservation_id
+      t.integer :guest_id
+      t.integer :listing_id
+      t.integer :host_id
+      t.integer :review_id
       t.timestamps
     end
   end
