ddc734c582b40e00960f060c4b09e412ad22fe70
JohnsonNYC <jkow95@gmail.com>
2020-04-14 13:25:41 -0400

    Updated show pages for User, Listing, and Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,12 @@
 class ReviewsController < ApplicationController
+    def show
+        @review = Review.find(params[:id])      
+    end
+
     def new
         @review=Review.new
     end 
+    
     def create 
         @review=Review.create(review_params)
         @review.user_id = current_user.id
