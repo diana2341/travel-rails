ee551df896eb17a795feb816f7b00bfd69476076
diana2341 <dianaponce2341@gmail.com>
2020-04-13 22:20:25 -0400

    reservations and reviews

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,31 @@
 class ReservationsController < ApplicationController
+    def new
+        @reservation=Reservation.new
+    end 
+    def create 
+      @reservation=Reservation.create(reservation_params)
+      @reservation.account_id = current_user.id
+     redirect_to confirmation_page_path(@reservation.id)
+    end 
+    def edit 
+        @reservation=Reservation.find(params[:id])
+    end 
+    def update 
+        @reservation=Reservation.find(params[:id])
+        @reservation.update(reservation_params)
+        @reservation.account_id = current_user.id
+        redirect_to confirmation_page_path(@reservation.id)
+    end 
+    def destroy
+        @reservation=Reservation.find(params[:id])
+        @reservation.destroy
+    end 
+    def confirmation_page
+        @user=User.find(params[:id])
+        
+    end
+    private 
+    def reservation_params
+    params.require(:reservation).permit(:guest_amount, :check_in,:check_out, :confirmation_number )
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+
+  belongs_to :guest, :class_name => "User"
+  belongs_to :reservation
+  belongs_to :listing
+
 end
