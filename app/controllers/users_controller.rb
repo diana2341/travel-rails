3b1a59bfb82a9f697e9dd705ae4fba194d4d296c
diana2341 <dianaponce2341@gmail.com>
2020-04-13 22:40:41 -0400

    fixed log ins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,6 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :age, :email, :password)
+        params.require(:user).permit(:first_name, :last_name, :age, :email, :password )
     end
 end
