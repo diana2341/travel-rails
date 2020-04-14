210bc0e1e19259b9585958fc49912824998cf139
JohnsonNYC <jkow95@gmail.com>
2020-04-13 22:15:46 -0400

    MVC created for User Listing and Location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+
+        flash[:errors] = @user.errors.full_messages
+        if @user.valid?
+            redirect_to user_path(@user)
+        else 
+            redirect_to new_user_path
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :age, :email, :password_digest)
+    end
 end
