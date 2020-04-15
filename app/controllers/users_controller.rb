b3f62475302e3a64f3eb572097e8ad801b9197e1
diana2341 <dianaponce2341@gmail.com>
2020-04-15 01:36:56 -0400

    uploads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
     def show 
-        # @user = User.find(params[:id])
-       @user= User.find_by(id:session[:user_id])
+        # if current_user
+          @user = User.find_by(id:params[:id])
+       
+        
+        # end
     end
 
     def new
